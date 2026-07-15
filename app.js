5f689fc5b88dbc553151a1636f85ef3d3cdd6148
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-15 13:01:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-document.getElementById("app").innerHTML = `
-<h2>🏐 Campionati</h2>
+window.onload = function () {
 
-<ul>
-  <li>Under 12 Femminile</li>
-  <li>Under 13 Femminile</li>
-  <li>Under 14 Femminile</li>
-  <li>Under 16 Femminile</li>
-  <li>Under 18 Femminile</li>
-  <li>Serie D</li>
-  <li>Volley S3</li>
-</ul>
-`;
+    document.getElementById("app").innerHTML = `
+        <h2>🏐 Campionati</h2>
+
+        <ul>
+            <li>Under 12 Femminile</li>
+            <li>Under 13 Femminile</li>
+            <li>Under 14 Femminile</li>
+            <li>Under 16 Femminile</li>
+            <li>Under 18 Femminile</li>
+            <li>Serie D</li>
+            <li>Volley S3</li>
+        </ul>
+    `;
+
+};

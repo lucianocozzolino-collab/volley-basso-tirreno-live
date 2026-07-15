6b0b20402bd66d92bc4d7643301deae877598aad
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-15 16:07:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
 window.onload = function () {
 
-  document.getElementById("lastUpdate").innerHTML =
-    "🕒 Ultimo aggiornamento: " +
-    new Date().toLocaleString("it-IT");
+    document.getElementById("lastUpdate").innerHTML =
+        "🕒 Ultimo aggiornamento: " +
+        new Date().toLocaleString("it-IT");
 
-  caricaHome();
+    caricaHome();
 };
 
 function caricaHome() {
 
-  document.getElementById("app").innerHTML = `
+    document.getElementById("app").innerHTML = `
 
-    <button onclick="aggiornaDati()">
-      🔄 Aggiorna Ora
-    </button>
+        <div class="card">
 
-    <h2>🏐 Campionati</h2>
+            <button onclick="aggiornaDati()">
+                🔄 Aggiorna Ora
+            </button>
 
-    <button onclick="apriCampionato('Under 12 Femminile')">
-      Under 12 Femminile
-    </button>
+            <h2>🏐 Campionati</h2>
 
-    <br><br>
+            <button onclick="apriCampionato('Under 12 Femminile')">
+                Under 12 Femminile
+            </button>
 
-    <button onclick="apriCampionato('Under 13 Femminile')">
-      Under 13 Femminile
-    </button>
+            <br><br>
 
-  `;
+            <button onclick="apriCampionato('Under 13 Femminile')">
+                Under 13 Femminile
+            </button>
+
+        </div>
+
+    `;
 }
 
 function apriCampionato(nome) {
 
-  document.getElementById("app").innerHTML = `
+    document.getElementById("app").innerHTML = `
 
-    <button onclick="caricaHome()">
-      ⬅ Torna
-    </button>
+        <button onclick="caricaHome()">
+            ⬅ Torna
+        </button>
 
-    <div class="card">
+        <div class="card">
 
-      <h2>${nome}</h2>
+            <h2>${nome}</h2>
 
-      <h3>📊 Classifica</h3>
-      <p>In caricamento...</p>
+            <p>
+                🌐 <a href="https://fipavonline.it/main/gare_girone/53943/1"
+                target="_blank">
+                Apri pagina ufficiale FIPAV
+                </a>
+            </p>
 
-      <h3>🏆 Risultati</h3>
-      <p>In caricamento...</p>
+            <h3>📊 Classifica</h3>
+            <p>In caricamento...</p>
 
-      <h3>📅 Calendario</h3>
-      <p>In caricamento...</p>
+            <h3>🏆 Risultati</h3>
+            <p>In caricamento...</p>
 
-      <p>
-        avonline.it/main/gare_girone/53943/1"
-           target="_blank">
-           🌐 Apri pagina FIPAV
-        </a>
-      </p>
+            <h3>📅 Calendario</h3>
+            <p>In caricamento...</p>
 
-    </div>
-  `;
+        </div>
+
+    `;
 }
 
 function aggiornaDati() {
-  location.reload();
+    location.reload();
 }

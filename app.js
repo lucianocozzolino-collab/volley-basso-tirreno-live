a0a29536424b6c04d3c9ad2d0219fe0a2ee89549
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-15 10:24:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+document.getElementById("app").innerHTML = `
+<h2>Campionati</h2>
 
+<ul>
+<li>Under 12</li>
+<li>Under 13</li>
+<li>Under 14</li>
+<li>Under 16</li>
+<li>Under 18</li>
+<li>Serie D</li>
+</ul>
+`;

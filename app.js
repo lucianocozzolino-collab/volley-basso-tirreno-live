13507ffdc2141d9814b9eeaa9fad2059a54560b7
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-15 10:29:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 document.getElementById("app").innerHTML = `
-<h2>Campionati</h2>
+
+<h2>🏐 Campionati</h2>
 
 <ul>
-<li>Under 12</li>
-<li>Under 13</li>
-<li>Under 14</li>
-<li>Under 16</li>
-<li>Under 18</li>
-<li>Serie D</li>
+  <li>Under 12 Femminile</li>
+  <li>Under 13 Femminile</li>
+  <li>Under 14 Femminile</li>
+  <li>Under 16 Femminile</li>
+  <li>Under 18 Femminile</li>
+  <li>Serie D</li>
+  <li>Volley S3</li>
 </ul>
+
 `;
+``

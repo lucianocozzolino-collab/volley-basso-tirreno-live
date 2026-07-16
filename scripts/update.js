26c7e18ca262842b95a4bcfb0bb042af0d95482f
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-16 08:59:37 +0200

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -8,6 +8,11 @@ const dati = {
       nome: "VOLLEY S3 Under 12 6x6 F - Girone D",
       id: "59767",
       url: "https://fipavonline.it/main/gare_girone/59767"
+    },
+    {
+      nome: "VOLLEY S3 Under 12 6x6 F - Girone A",
+      id: "59764",
+      url: "https://fipavonline.it/main/gare_girone/59764"
     }
   ]
 };

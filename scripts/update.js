3ec615aeff9f0fa5de07c84b4c557d2f4cfc9a25
lucianocozzolino-collab <luciano.cozzolino@gmail.com>
2026-07-16 09:17:10 +0200

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,28 +1,13 @@
 const fs = require("fs");
 
-async function main() {
+const test = {
+  data: new Date().toISOString(),
+  messaggio: "Workflow funzionante"
+};
 
-  const id = 59767;
+fs.writeFileSync(
+  "data/test.json",
+  JSON.stringify(test, null, 2)
+);
 
-  const url =
-    `https://fipavonline.it/main/gare_girone/${id}`;
-
-  const response =
-    await fetch(url);
-
-  const html =
-    await response.text();
-
-  fs.mkdirSync("data", {
-    recursive: true
-  });
-
-  fs.writeFileSync(
-    "data/test-html.html",
-    html
-  );
-
-  console.log("HTML salvato");
-}
-
-main();
+console.log("OK");
